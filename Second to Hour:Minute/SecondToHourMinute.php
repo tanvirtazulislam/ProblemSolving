40f38bf65fb573442d456ef7fc045b01c0f07434
Tanvir Tazul Islam <tanvirtazul@gmail.com>
2023-10-27 18:27:21 +0600

    Update SecondToHourMinute.php

--- Second to Hour:Minute/SecondToHourMinute.php
+++ Second to Hour:Minute/SecondToHourMinute.php
@@ -1,1 +1,22 @@
 
+<?php
+    # Write your PHP code from here
+
+    fscanf(STDIN, "%d", $seconds);
+    //echo $seconds."\n";
+
+    $hours = 0;
+    $minutes = 0;
+
+    $minutes = intval($seconds/60);
+    //echo $minutes."\n";
+    //echo var_dump($minutes);
+
+    if($minutes >= 60){
+        $hours = intval($minutes/60);
+        $minutes = $minutes%60;
+    }
+
+    echo $hours.":".$minutes;
+
+?>
