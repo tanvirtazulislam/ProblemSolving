8e9321b29ab194ade445fe8b267c811552f032fa
Tanvir Tazul Islam <tanvirtazul@gmail.com>
2023-11-02 15:00:17 +0600

    Update FindDiscount.php

--- Find Discount/FindDiscount.php
+++ Find Discount/FindDiscount.php
@@ -1,1 +1,15 @@
+<?php
+    # Write your PHP code from here
 
+    function discounterPrice(int $p, int $d){
+        //echo $p." ".$d."\n";
+        $discountedPrice = $p - ($p*$d/100);
+        return $discountedPrice;
+    }
+
+    fscanf(STDIN, "%d %d", $price, $discount);
+    //echo $price." ".$discount."\n";
+
+    printf("Price: %.2f", discounterPrice($price, $discount));
+    
+?>
