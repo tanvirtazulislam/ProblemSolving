3c0140fd3aecbbd556abe2996a5c1b3c8671bce1
Tanvir Tazul Islam <tanvirtazul@gmail.com>
2023-11-02 15:02:49 +0600

    Update BuildToyCars.php

--- Build Toy Cars/BuildToyCars.php
+++ Build Toy Cars/BuildToyCars.php
@@ -1,1 +1,7 @@
-
+<?php
+    # Write your PHP code from here
+    fscanf(STDIN, "%d %d %d", $wheels, $carBodies, $figures);
+    //echo $wheels." ".$carBodies." ".$figures."\n";
+    
+    echo min(intval($wheels/4), intval($carBodies/1), intval($figures/2));
+?>
