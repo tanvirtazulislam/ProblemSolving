3c2d1418f94ede3f2076069bf4fce0f7f8df24c1
Tanvir Tazul Islam <tanvirtazul@gmail.com>
2023-11-09 12:59:48 +0600

    Update StringRotation.php

--- String Rotation/StringRotation.php
+++ String Rotation/StringRotation.php
@@ -1,1 +1,40 @@
+?php
+    # Write your PHP code from here
+    fscanf(STDIN, "%s %s", $string, $rotatedString);
+    //echo $string." ".$rotatedString."\n";
+    
+    if(stringRotation($string, $rotatedString))
+        echo "True";
+    else
+        echo "False";
 
+
+    function stringRotation($string, $rotatedString){
+
+        $sizeString = strlen($string);
+        //echo $sizeString."\n";
+
+        $sizeRotatedString = strlen($rotatedString);
+        //echo $sizeRotatedString."\n";
+
+        $temp = '';
+        
+        if($sizeString != $sizeRotatedString){
+            return false;
+        }
+        else if($string == $rotatedString)
+            return true;
+        else{
+            for($i = 0; $i < $sizeString; $i++){
+                $temp = $string[0]; 
+                for($j = 1; $j < $sizeString; $j++){
+                    $string[$j-1] = $string[$j];
+                }
+                $string[$sizeString-1] = $temp;
+
+                if($string == $rotatedString)
+                    return true;
+            }
+        } 
+    }
+?>
