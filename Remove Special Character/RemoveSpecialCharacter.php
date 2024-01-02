43c3ed8db2e81380af385f1526f471343cee5153
Tanvir Tazul Islam <tanvirtazul@gmail.com>
2024-01-02 15:52:57 +0600

    Update RemoveSpecialCharacter.php

--- Remove Special Character/RemoveSpecialCharacter.php
+++ Remove Special Character/RemoveSpecialCharacter.php
@@ -1,1 +1,21 @@
+<?php
+    # Write your PHP code from here
 
+    // characters fall in a range of 65 to 122 in ASCII value.
+    // any character outside of this range will be a special character
+
+    $input = trim(fgets(STDIN));
+    // echo $input;
+
+    $string = "";
+    for($i = 0; $i < strlen($input); $i++){
+        $value = ord($input[$i]);
+        if($value >= 65 && $value <= 122){
+            $string .= $input[$i];
+        }else{
+            continue;
+        }
+    }
+    echo $string;
+    
+?>
