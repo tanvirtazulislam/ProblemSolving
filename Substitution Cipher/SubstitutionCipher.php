fb8ceab2f412f2de2cdee096a086c5e2f4004f9e
Tanvir Tazul Islam <tanvirtazul@gmail.com>
2024-01-02 15:48:29 +0600

    Update SubstitutionCipher.php

--- Substitution Cipher/SubstitutionCipher.php
+++ Substitution Cipher/SubstitutionCipher.php
@@ -1,1 +1,16 @@
+<?php
+    # Write your PHP code from here
+    
+    //$input = trim(fgets(STDIN));
+    fscanf(STDIN, "%s %d", $string, $num);
+    // print $string."\n".$num;
+    
+    $updatedString = "";
+    for($i = 0; $i < strlen($string); $i++){
+        $updatedString = $updatedString.chr(ord($string[$i])+$num);
+        // echo $updatedString;
+    }
+    echo $updatedString;
 
+    
+?>
